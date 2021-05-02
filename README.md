5320d0671149214c5942d371139293629fb8e244
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-02 14:12:38 +0300

    added infrastructure code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Import an Existing S3 Bucket in AWS CDK
+
+A repository for an article on
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-import-existing-s3-bucket)
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Replace the placeholders - `YOUR_EXTERNAL_BUCKET_NAME` with the actual value
+   for the bucket you want to import
+
+4. Synth the stack and look at the console.log output
+
+```bash
+npx cdk synth
+```
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,34 @@
+import * as iam from '@aws-cdk/aws-iam';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from '@aws-cdk/core';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const importedBucketFromName = s3.Bucket.fromBucketName(
+      this,
+      'imported-bucket-from-name',
+      'YOUR_EXTERNAL_BUCKET_NAME',
+    );
+
+    console.log('bucket name 👉', importedBucketFromName.bucketName);
+    console.log('bucket arn 👉', importedBucketFromName.bucketArn);
+
+    importedBucketFromName.grantRead(new iam.AccountRootPrincipal());
+
+    const importedBucketFromArn = s3.Bucket.fromBucketArn(
+      this,
+      'imported-bucket-from-arn',
+      'arn:aws:s3:::YOUR_EXTERNAL_BUCKET_NAME',
+    );
+
+    const importedBucketfromAttributes = s3.Bucket.fromBucketAttributes(
+      this,
+      'imported-bucket-from-attributes',
+      {
+        bucketArn: 'arn:aws:s3:::YOUR_EXTERNAL_BUCKET_NAME',
+      },
+    );
+  }
+}
